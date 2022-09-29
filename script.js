4a696a790160a6d377ec0f4b7600ffbd28d7bf90
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-09-28 21:56:16 -0300

    update scripts

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function loaspk(){
+    let url = 'https://pokeapi.co/api/v2/pokemon/1'
+    fetch(url)
+    
+    .then((responde) =>{
+         return Response.json();
+        
+    }
+    )
+    
+    
+    .then((data) => {
+        // o q faremos com o formato json
+
+
+    })
+    .catch((erro) => {
+        console.log('Erro:' + erro);
+    })
+
+
 console.clear();
 console.log("Funciona")
 
